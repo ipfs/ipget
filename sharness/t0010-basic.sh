15d71e330a606dd1d3008113d3a9ef63e9aff268
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-09-13 22:30:28 -0700

    Use "shasum" instead of "sha1sum" for OSX compatibility.

--- sharness/t0010-basic.sh
+++ sharness/t0010-basic.sh
@@ -7,14 +7,14 @@ test_description="test the ipget command"
 test_expect_success "retrieve a known popular single file" "
     ipget QmQ2r6iMNpky5f1m4cnm3Yqw8VSvjuKpTcK1X7dBR1LkJF/cat.gif &&
     echo 'c5ea0d6cacf1e54635685803ec4edbe0d4fe8465' > expected &&
-    sha1sum cat.gif | cut -d ' ' -f 1 > actual &&
+    shasum cat.gif | cut -d ' ' -f 1 > actual &&
     diff expected actual
 "
 
 test_expect_success "retrieve a known popular file with -o" "
     ipget -o meow.gif QmQ2r6iMNpky5f1m4cnm3Yqw8VSvjuKpTcK1X7dBR1LkJF/cat.gif &&
     echo 'c5ea0d6cacf1e54635685803ec4edbe0d4fe8465' > expected &&
-    sha1sum meow.gif | cut -d ' ' -f 1 > actual &&
+    shasum meow.gif | cut -d ' ' -f 1 > actual &&
     diff expected actual
 "
 
