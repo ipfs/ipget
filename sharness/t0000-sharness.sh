6a17885afb09a3a2fcd1364ffdd3a1c89012373c
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-09-13 21:10:58 -0700

    Simple sharness test.

--- sharness/t0000-sharness.sh
+++ sharness/t0000-sharness.sh
@@ -1,32 +1,14 @@
 #!/bin/sh
 
-test_description="Show basic features of Sharness"
+test_description="test the ipget command"
 
 . ./lib/sharness/sharness.sh
 
-test_expect_success "Success is reported like this" "
-    echo hello world | grep hello
-"
-
-test_expect_success "Commands are chained this way" "
-    test x = 'x' &&
-    test 2 -gt 1 &&
-    echo success
-"
-
-return_42() {
-    echo "Will return soon"
-    return 42
-}
-
-test_expect_success "You can test for a specific exit code" "
-    test_expect_code 42 return_42
-"
-
-test_expect_failure "We expect this to fail" "
-    test 1 = 2
+test_expect_success "retrieve a cat gif" "
+    ipget QmQ2r6iMNpky5f1m4cnm3Yqw8VSvjuKpTcK1X7dBR1LkJF/cat.gif &&
+    echo 'c5ea0d6cacf1e54635685803ec4edbe0d4fe8465' > expected &&
+    sha1sum cat.gif | cut -d ' ' -f 1 > actual &&
+    diff expected actual
 "
 
 test_done
-
-# vi: set ft=sh :
