1449a81134a9a9f454c0523734698a642892183c
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-09-13 21:18:42 -0700

    Add test for -o switch.

--- sharness/t0000-sharness.sh
+++ sharness/t0000-sharness.sh
@@ -4,11 +4,22 @@ test_description="test the ipget command"
 
 . ./lib/sharness/sharness.sh
 
-test_expect_success "retrieve a cat gif" "
+test_expect_success "retrieve a known popular single file" "
     ipget QmQ2r6iMNpky5f1m4cnm3Yqw8VSvjuKpTcK1X7dBR1LkJF/cat.gif &&
     echo 'c5ea0d6cacf1e54635685803ec4edbe0d4fe8465' > expected &&
     sha1sum cat.gif | cut -d ' ' -f 1 > actual &&
     diff expected actual
 "
+
+test_expect_success "retrieve a known popular file with -o" "
+    ipget -o meow.gif QmQ2r6iMNpky5f1m4cnm3Yqw8VSvjuKpTcK1X7dBR1LkJF/cat.gif &&
+    echo 'c5ea0d6cacf1e54635685803ec4edbe0d4fe8465' > expected &&
+    sha1sum meow.gif | cut -d ' ' -f 1 > actual &&
+    diff expected actual
+"
+
+# TODO(noffle): recursive retrieval (directory)
+
+# TODO(noffle): start a daemon, add + fetch a unique hash from it
 
 test_done
