9b18e4d8a0532a2e71aaf675f959626fa8d7d859
Dominic Della Valle <ddvpublic@gmail.com>
2018-02-13 11:28:00 -0500

    Add IPFS URL+URI parser
    Accept HTTP gateway URLs and IPFS URIs as arguments

---
+++ sharness/t0030-arguments.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+test_description="test the ipget argument parser"
+
+. ./lib/sharness/sharness.sh
+
+test_expect_success "retrieve a known popular single file with a (HTTP) gateway URL" "
+    ipget http://ipfs.io/ipfs/QmQ2r6iMNpky5f1m4cnm3Yqw8VSvjuKpTcK1X7dBR1LkJF/cat.gif &&
+    echo 'c5ea0d6cacf1e54635685803ec4edbe0d4fe8465' > expected &&
+    shasum cat.gif | cut -d ' ' -f 1 > actual &&
+    diff expected actual
+"
+
+test_expect_success "retrieve a known popular single file with browser protocol URI" "
+    ipget ipfs://QmQ2r6iMNpky5f1m4cnm3Yqw8VSvjuKpTcK1X7dBR1LkJF/cat.gif &&
+    echo 'c5ea0d6cacf1e54635685803ec4edbe0d4fe8465' > expected &&
+    shasum cat.gif | cut -d ' ' -f 1 > actual &&
+    diff expected actual
+"
+
+test_expect_failure "don't allow non-(HTTP)gateway URLS" "
+    ipget ftp://ipfs.io/ipfs/QmQ2r6iMNpky5f1m4cnm3Yqw8VSvjuKpTcK1X7dBR1LkJF/cat.gif
+"
+
+test_done
