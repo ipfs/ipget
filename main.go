4650f4a786a51b082d01c11c41597b063796f914
Steven Allen <steven@stebalien.com>
2021-04-21 22:46:34 -0700

    release v0.7.0

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "ipget"
 	app.Usage = "Retrieve and save IPFS objects."
-	app.Version = "0.6.1"
+	app.Version = "0.7.0"
 	app.Flags = []cli.Flag{
 		&cli.StringFlag{
 			Name:    "output",
