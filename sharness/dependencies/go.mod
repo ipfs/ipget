abce534c3f192a6529ad60dce108b04ca2e64637
gammazero <11790789+gammazero@users.noreply.github.com>
2024-08-14 09:25:33 -0700

    fix sharness test

--- sharness/dependencies/go.mod
+++ sharness/dependencies/go.mod
@@ -2,8 +2,6 @@ module github.com/ipfs/ipget/sharness/dependencies
 
 go 1.22
 
-toolchain go1.22.5
-
 require (
 	github.com/chriscool/go-sleep v0.0.0-20150606204034-743ab5f1bb48
 	github.com/ipfs/ipget v0.9.2
--- sharness/t0040-unixfs-mode-modtime.sh
+++ sharness/t0040-unixfs-mode-modtime.sh
@@ -11,40 +11,50 @@ test_launch_ipfs_daemon
 
 test_expect_success "create test file with mode and mtime" '
 	echo "hello ipget" > test_file &&
-	ipfs add -q --mode=0664 --mtime=660000000 test_file > hash
+	ipfs add -q --mode=0666 --mtime=660000000 test_file > hash
 	cat hash
 '
 test_expect_success "retrieve file with mode and mtime" '
     ipget -o data.txt --node=local "/ipfs/$(<hash)" &&
     test_cmp test_file "data.txt" &&
-    if test $(uname -s) = "Darwin"; then
+    case $(uname -s) in
+    Linux|FreeBSD)
+        stat --format="%Y %a" data.txt > out &&
+        echo "660000000 666" > expect && 
+        test_cmp expect out
+        ;;
+    Darwin)
         stat -f "%m %p" data.txt > out &&
-        echo "660000000 100664" > expect &&
+        echo "660000000 100666" > expect &&
         test_cmp expect out
-    elif test $(uname -s) = "Linux"; then
-        stat --format="%a" data.txt > out &&
-        echo "664" > expect2 && 
-        test_cmp expect out
-    fi
+        ;;
+    *)
+        echo "unsupported system: $(uname)"
+    esac
 '
 
 test_expect_success "create a test directory" '
     mkdir test_dir2 &&
     cp test_file test_dir2/data.txt &&
-    ipfs add --mode=0775 --mtime=660000000 -rQ test_dir2 > dir_hash
+    ipfs add --mode=0777 --mtime=660000000 -rQ test_dir2 > dir_hash
 '
 
-test_expect_success "retrieve a directory" '
+test_expect_success "retrieve a directory with mode and mtime" '
     ipget --node=local -o got_dir "/ipfs/$(<dir_hash)" &&
-    if test $(uname -s) = "Darwin"; then
+    case $(uname -s) in
+    Linux|FreeBSD)
+        stat --format="%Y %a" got_dir > out2 &&
+        echo "660000000 777" > expect2 &&
+        test_cmp expect2 out2
+        ;;
+    Darwin)
         stat -f "%m %p" got_dir > out2 &&
-        echo "660000000 40775" > expect2 &&
+        echo "660000000 40777" > expect2 &&
         test_cmp expect2 out2
-    elif test $(uname -s) = "Linux"; then
-        stat --format="%a" got_dir > out2 &&
-        echo "775" > expect2 &&
-        test_cmp expect2 out2
-    fi   
+        ;;
+    *)
+        echo "unsupported system: $(uname)"
+    esac
 '
 
 # kill the local ipfs node
